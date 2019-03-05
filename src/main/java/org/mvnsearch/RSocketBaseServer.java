cfdadbcb7ff37dc0bbfe58fe9b7f257c27c020f3
linux_china <libing.chen@gmail.com>
2019-03-05 12:01:07 -0800

    polishing

--- src/main/java/org/mvnsearch/RSocketBaseServer.java
+++ src/main/java/org/mvnsearch/RSocketBaseServer.java
@@ -1,15 +1,10 @@
 package org.mvnsearch;
 
-import io.rsocket.AbstractRSocket;
-import io.rsocket.Payload;
 import io.rsocket.RSocketFactory;
 import io.rsocket.transport.netty.server.TcpServerTransport;
-import io.rsocket.util.DefaultPayload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
-
-import java.util.Date;
 
 /**
  * RSocket base server
@@ -22,17 +17,7 @@ public abstract class RSocketBaseServer {
     public void start() {
         log.info("Begin to start the rsocket server");
         RSocketFactory.receive()
-                .acceptor(
-                        (setupPayload, reactiveSocket) ->
-                                Mono.just(
-                                        new AbstractRSocket() {
-
-                                            @Override
-                                            public Mono<Payload> requestResponse(Payload p) {
-                                                System.out.println("payload:" + p.getDataUtf8());
-                                                return Mono.just(DefaultPayload.create(serverId() + ":" + new Date()));
-                                            }
-                                        }))
+                .acceptor((setupPayload, reactiveSocket) -> Mono.just(new SimpleResponderHandler()))
                 .transport(TcpServerTransport.create("0.0.0.0", port()))
                 .start()
                 .subscribe();

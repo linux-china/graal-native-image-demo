c56dced7120998c9ac92e5d3fe52c3f0e32a6bd2
linux_china <libing.chen@gmail.com>
2019-06-06 14:24:44 -0700

    add proxy test

--- src/main/java/org/mvnsearch/RSocketAppServer.java
+++ src/main/java/org/mvnsearch/RSocketAppServer.java
@@ -1,5 +1,9 @@
 package org.mvnsearch;
 
+import org.mvnsearch.proxy.DemoService;
+import org.mvnsearch.proxy.MyProxyHandler;
+
+import java.lang.reflect.Proxy;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -24,4 +28,8 @@ public class RSocketAppServer extends RSocketBaseServer {
         return "server1";
     }
 
+    public static void proxyTest() {
+        DemoService demoService = (DemoService) Proxy.newProxyInstance(RSocketAppServer.class.getClassLoader(), new Class<?>[]{DemoService.class}, new MyProxyHandler());
+        demoService.hello("RSocket");
+    }
 }
